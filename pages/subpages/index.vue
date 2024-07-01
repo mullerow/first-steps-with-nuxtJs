8ae74bfd7542f7970a45f387676f1e9b290da89a
mullerow <maximilian.kohler@posteo.de>
2024-07-01 11:07:06 +0200

    add some subfolder to learn routing with nuxt

---
+++ pages/subpages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Index of Subpages</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ pages/subpages/subpage1.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Subpage 1</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
